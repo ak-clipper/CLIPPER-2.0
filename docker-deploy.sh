028836910bd094e2915c27a6b14819affd8f0c7a
AndersDkr <>
2024-05-28 12:49:12 +0200

    Update docker deploy script to deploy to ECR

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,1 +1,4 @@
+aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 058264536995.dkr.ecr.eu-central-1.amazonaws.com
 docker build -t clipper .
+docker tag clipper:latest 058264536995.dkr.ecr.eu-central-1.amazonaws.com/clipper:latest
+docker push 058264536995.dkr.ecr.eu-central-1.amazonaws.com/clipper:latest
